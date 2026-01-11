88d68ee515ee5a18ada03f7acb386fd94a3d7425
Priyadarshan Garg <gargpriyadarshan18@gmail.com>
2026-01-11 08:21:21 +0530

    Did some compression setting in init method

--- src/CameraHandler.cpp
+++ src/CameraHandler.cpp
@@ -6,7 +6,22 @@ CameraHandler::CameraHandler() {
 }
 
 bool CameraHandler::initCamera(int id) {
-    return cap.open(id); // Ab ye 'cap' ko pehchan lega
+	cap.open(id);
+	if (!cap.isOpened()) {
+        return false;
+    }
+
+	cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    /*
+	* UDB Camers YUYV(Color Model) format me data deti hai jo uncompressed hota hai
+	* jo usb bandwidth zyada leta hai aur processing slow kar deta hai
+	* to isliye hum MJPG format set kar rahe hai jisse camera compressed frames dega
+    */
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720); 
+	cap.set(cv::CAP_PROP_FPS, 30);
+	cap.set(cv::CAP_PROP_BUFFERSIZE, 1); // 1 isliye mujhe latest frame mile old nahi
+	return true;
 }
 
 cv::Mat CameraHandler::getFrame() {
