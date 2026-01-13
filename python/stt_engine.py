4f4f17e1ead71bcd6c68903a6fdda2dd197c0cc4
Priyadarshan Garg <gargpriyadarshan18@gmail.com>
2026-01-13 22:30:42 +0530

    instead increasing IO operations changed audio into np array cuz the model was also gonna process it in binary

--- python/stt_engine.py
+++ python/stt_engine.py
@@ -1,10 +1,8 @@
 from faster_whisper.transcribe import Segment
 import speech_recognition as sr
 from faster_whisper import WhisperModel
-import os
-
+import numpy as np
 import time
-
 
 Model_Size = "medium"
 Device = "cuda"
@@ -12,27 +10,28 @@ Compute_type = "float16"
 
 start_time = time.time()
 
-model =WhisperModel(Model_Size, device=Device, compute_type=Compute_type)
+model = WhisperModel(Model_Size, device=Device, compute_type=Compute_type)
 print(f"Model loaded in {time.time() - start_time} seconds")
 
-def  listen_and_transcribe():
+def listen_and_transcribe():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source, duration=0.5)
         try:
+            print("Listening...")
             audio = recognizer.listen(source, timeout=5, phrase_time_limit=10)
-            audio_raw_data = audio.get_wav_data()
-            with open("temp_audio.wav", "wb") as f:
-                f.write(audio_raw_data)
-            Segments, info = model.transcribe("temp_audio.wav", beam_size=5)
-            text = " ".join([Segment.text for Segment in Segments])
+            
+            raw_data = audio.get_raw_data(convert_rate=16000, convert_width=2)
+            audio_np = np.frombuffer(raw_data, dtype=np.int16).astype(np.float32) / 32768.0
+            
+            segments, info = model.transcribe(audio_np, beam_size=5)
+            text = " ".join([segment.text for segment in segments])
 
             if text.strip():
                 print(text)
                 return text
             else:
-                  print(f"Couldn't process audio properly")
-                  return None
+                return None
         except sr.WaitTimeoutError:
             print("Listening timed out while waiting for phrase to start")
             return None
@@ -48,16 +47,12 @@ def check_exit(text):
         return True
     return False
 
-
 if __name__ == "__main__":
     try:
         while True:
             text = listen_and_transcribe()
-
             if check_exit(text):
-               break
+                print("Exiting...")
+                break
     except KeyboardInterrupt:
         print("Program interrupted by user.")
-    finally:
-        if os.path.exists("temp_audio.wav"):
-            os.remove("temp_audio.wav")
