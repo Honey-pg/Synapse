193d81e09f213aa2ec13917d4ddffb53d22d2e3c
Priyadarshan Garg <gargpriyadarshan18@gmail.com>
2026-01-18 15:34:35 +0530

    The main Synapse file where it handle the processing.

---
+++ python/engine/main.py
@@ -0,0 +1,35 @@
+
+from tkinter import SEL
+from python.engine import stt_engine
+from stt_engine import STT_Engine
+from tts_engine import TTS_Engine
+from llm_engine import LLM_Engine
+import colorama
+
+colorama.init(autoreset=True)
+
+class Synapse :
+    def __init__(self):
+        print(colorama.Fore.CYAN + f"Initializing Synapse AI Engine...")
+
+        self.ear = STT_Engine()
+        self.mouth  = TTS_Engine()
+        self.brain = LLM_Engine()
+        self.mouth.speak("System Online. Ready for commands")
+
+    def start(self) :
+        while True :
+            command = self.ear.listen()
+            if command :
+                if stt_engine.check_exit(command) : 
+                    self.mouth.speak("Goodbye, see you latter !")
+                    break;
+                ai_response = self.brain.chat(self,command)
+                self.mouth.speak(ai_response)
+
+if __name__ == "__main__" :
+    try :
+        app = Synapse()
+        app.start()
+    except KeyboardInterrupt:
+        print(f"Interuppted by user")
